f044a04dfcd2945d3e152f8e0fb50a256d7a2413
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-05-09 14:47:01 +0200

    added part 1

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import MapScreen from './screens/MapScreen';
+import HomeScreen from './screens/HomeScreen';
+import ChatScreen from './screens/ChatScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import pseudo from './reducers/pseudo';
+
+const store = createStore(combineReducers({pseudo}));
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const BottomNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          let iconName;
+
+          if (route.name == 'Map') {
+            iconName = 'ios-navigate';
+          } else if (route.name == 'Chat') {
+            iconName = 'ios-chatbubbles';
+          }
+  
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+        })}
+      tabBarOptions={{
+        activeTintColor: '#eb4d4b',
+        inactiveTintColor: '#FFFFFF',
+        style: {
+          backgroundColor: '#130f40',
+        }
+      }}
+    >
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Chat" component={ChatScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
---
+++ screens/ChatScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
+import {Button, ListItem, Input} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function ChatScreen() {
+  return (
+    <View style={{flex:1}}>
+       
+      <ScrollView style={{flex:1, marginTop: 50}}>
+        <ListItem>
+          <ListItem.Content>
+            <ListItem.Title>Parfait et toi ?</ListItem.Title>
+            <ListItem.Subtitle>Alex</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+        <ListItem>
+          <ListItem.Content>
+            <ListItem.Title>Coucou ça roule ?</ListItem.Title>
+            <ListItem.Subtitle>John</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      </ScrollView>
+
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+          <Input
+              containerStyle = {{marginBottom: 5}}
+              placeholder='Your message'
+          />
+          <Button
+              icon={
+                  <Icon
+                  name="envelope-o"
+                  size={20}
+                  color="#ffffff"
+                  />
+              } 
+              title="Send"
+              buttonStyle={{backgroundColor: "#eb4d4b"}}
+              type="solid"
+          />
+      </KeyboardAvoidingView>
+        
+    </View>
+  );
+}
